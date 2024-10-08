ede4338f2dd512ffd7747205230664eb328b470f
ChristianGalipo1 <s331996@studenti.polito.it>
2024-10-08 18:22:21 +0200

    server and client APi, method insertTicket in ticketDAO, method getServices in serviceDAO

--- server/Index.mjs
+++ server/Index.mjs
@@ -1,6 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import { check, validationResult } from 'express-validator';
+import { insertTicket } from './src/dao/ticketDAO.mjs';
+import { getServices } from './src/dao/serviceDAO.mjs';
 
 const app = express();
 const port = 3001;
@@ -15,5 +18,43 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+
+//ticketAPI
+app.post('/api/ticket',[ 
+    check('number').isNumeric().notEmpty().custom(value => value > 0),
+    check('esimatedTime').isNumeric().notEmpty().custom(value => value > 0),
+    check('serviceId').isNumeric().notEmpty().custom(value => value > 0),
+    check('timeId').isNumeric().notEmpty().custom(value => value > 0)
+], async (req, res)=>{
+    const errors = validationResult(req);
+
+    try{
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
+        const number = req.body.number;
+        const esimatedTime = req.body.esimatedTime;
+        const serviceId = req.body.serviceId;
+        const timeId = req.body.timeId;
+
+        const ticketNumber = await insertTicket(number, esimatedTime, serviceId, timeId);
+        return res.status(200).json(ticketNumber).end();
+    }catch(error) {
+        console.error('Error in ticket creation:', error);
+        return res.status(500).json({ error: 'Internal Server Error' }).end();
+    }
+})
+
+//serviceAPI
+app.get('/api/service', async(req, res)=>{
+    try {
+        const listServices = await getServices();
+        res.status(200).json(listServices)
+    } catch (error) {
+        console.error('Error in proposal creation:', error);
+        return res.status(500).json({ error: 'Internal Server Error' }).end();
+    }
+})
 
 app.listen(port, () => { console.log(`Server started at http://localhost:${port}`); });
