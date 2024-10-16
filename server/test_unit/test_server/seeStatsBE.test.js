afd060a39153af2603b9d7bfe6b38943b8da7f07
Matteo D'Alicarnasso s327075 <s327075@studenti.polito.it>
2024-10-16 18:37:23 +0200

    first server tests for seeStats

--- server/test_unit/test_server/seeStatsBE.test.js
+++ server/test_unit/test_server/seeStatsBE.test.js
@@ -2,26 +2,13 @@
 import { test, expect, jest, afterEach } from "@jest/globals"
 import request from 'supertest'
 import express from "express"
-import { app, server } from "../../Index.mjs"
-import * as dao from "../../src/dao/statsDAO.mjs"
-//import { getStats2Dmonth, getStats2Dweek, getStats2Dday, getStats3Dmonth, getStats3Dweek, getStats3Dday } from './src/dao/statsDAO.mjs';
-//import getStats2Dmonth from "../../src/dao/statsDAO.mjs"
+//import { app, server } from "../../Index.mjs"
+import app from "../../Index.mjs";
+import { getStats2Dmonth, getStats2Dweek, getStats2Dday, getStats3Dmonth, getStats3Dweek, getStats3Dday } from '../../src/dao/statsDAO.mjs';
+
+jest.mock('../../src/dao/statsDAO.mjs');
 
 
-//const baseURL = "/ezelectronics"
-//let app: express.Application;
-
-jest.mock('../../src/dao/statsDAO');
-
-
-/*jest.mock('../../src/dao/statsDAO.mjs', () => ({
-    getStats2Dmonth: jest.fn(),
-}));*/
-
-/*jest.mock(dao, () => ({
-    __esModule: true,
-    getStats2Dmonth: jest.fn().mockResolvedValueOnce("prova"),
-}));*/
 
 describe("Server routes tests", () => {
 
@@ -30,9 +17,9 @@ describe("Server routes tests", () => {
         jest.resetAllMocks();
     });
 
-    afterEach((done) => {
-        server.close(done);
-    })
+    /*afterEach((done) => {
+        //server.close(done);
+    })*/
 
     /* ************************ /api/Stats2Dmonth ************************* */
     describe("/api/Stats2Dmonth", () => {
@@ -50,29 +37,44 @@ describe("Server routes tests", () => {
                 { year: '2024', month: '10', serviceName: 'spedizioni', totalTickets: '31' },
                 { year: '2024', month: '10', serviceName: 'servizio3', totalTickets: '7' },
             ]
-
-            //getStats2Dmonth.mockResolvedValue(DBdata);
-            dao.getStats2Dmonth.mockResolvedValue(DBdata);
-            //jest.spyOn(dao, 'getStats2Dmonth').mockResolvedValueOnce(resultData);
-            //dao.getStats2Dmonth = jest.fn().mockResolvedValueOnce(resultData);
-            //getStats2Dmonth = jest.fn().mockResolvedValueOnce(resultData);
-            //jest.spyOn(dao, "getStats2Dmonth").mockResolvedValueOnce(resultData)
+            getStats2Dmonth.mockResolvedValue(DBdata);
 
             const response = await request(app)
                 .get('/api/Stats2Dmonth')
                 .query({ startDate: '2024-10-10', endDate: '2024-10-24' })
-                //.expect('Content-Type', 'json')
                 .expect(200);
 
-            // Controlla che il risultato sia stato filtrato correttamente
             expect(response.body).toStrictEqual(resultData);
-
-            // Verifica che getStats2Dmonth sia stato chiamato una volta
             expect(getStats2Dmonth).toHaveBeenCalledTimes(1);
 
-            // Verifica che isDateInRange sia stato chiamato per ogni item in mockStats
-            expect(isDateInRange).toHaveBeenCalledTimes(3);
 
+        });
+    });
+
+    describe("/api/Stats3Dmonth", () => {
+        test("It should return a 200 success code", async () => {
+
+            let DBdata = [
+                { year: '2023', month: '10',counterN:'1', serviceName: 'servizio1', totalTickets: '99' },
+                { year: '2024', month: '10',counterN:'1', serviceName: 'SPID', totalTickets: '20' },
+                { year: '2024', month: '10',counterN:'2', serviceName: 'spedizioni', totalTickets: '31' },
+                { year: '2024', month: '10',counterN:'3', serviceName: 'servizio3', totalTickets: '7' },
+                { year: '2024', month: '11',counterN:'1', serviceName: 'SPID', totalTickets: '26' },
+            ]
+            const resultData = [
+                { year: '2024', month: '10',counterN:'1', serviceName: 'SPID', totalTickets: '20' },
+                { year: '2024', month: '10',counterN:'2', serviceName: 'spedizioni', totalTickets: '31' },
+                { year: '2024', month: '10',counterN:'3', serviceName: 'servizio3', totalTickets: '7' },
+            ]
+            getStats3Dmonth.mockResolvedValue(DBdata);
+
+            const response = await request(app)
+                .get('/api/Stats3Dmonth')
+                .query({ startDate: '2024-10-10', endDate: '2024-10-24' })
+                .expect(200);
+
+            expect(response.body).toStrictEqual(resultData);
+            expect(getStats3Dmonth).toHaveBeenCalledTimes(1);
         });
     });
 });
